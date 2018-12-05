82464fb7d8fb76f1a60d2a14cbc79ecc316840da
gustafeden <gustafeden@gmail.com>
2018-12-05 15:13:05 +0100

    added tests for insertFirst() in Doublelinkedlink

---
+++ Datastrukturer.test/Doublelinkedlist-tests.cpp
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "../Datastrukturer/Doublelinkedlist.h"
+TEST(Doublelinkedlisttests, WhenAddingOneThingToListHeadShouldBeWhatYouAdded) {
+	Doublelinkedlist<int> testlist;
+	testlist.InsertFirst(123, 1);
+	EXPECT_EQ(123, testlist.getHead());
+  
+}
+TEST(Doublelinkedlisttests, WhenAddingTwoThingsToListHeadShouldBeWhatYouAddedLast) {
+	Doublelinkedlist<int> testlist;
+	testlist.InsertFirst(123, 1);
+	testlist.InsertFirst(124, 2);
+	EXPECT_EQ(124, testlist.getHead());
+
+}
+TEST(Doublelinkedlisttests, WhenAddingTwoThingsToListTailShouldBeWhatYouAddedFirst) {
+	Doublelinkedlist<int> testlist;
+	testlist.InsertFirst(123, 1);
+	testlist.InsertFirst(124, 2);
+	EXPECT_EQ(123, testlist.getTail());
+
+}
