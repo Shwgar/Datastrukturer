310ec3582898501fd71148c223458c2db810f4d8
Martin <martin@swedishgamers.com>
2018-12-05 15:41:21 +0100

    Linkedlist base is ready

--- Datastrukturer/main.cpp
+++ Datastrukturer/main.cpp
@@ -2,17 +2,30 @@
 #include<string>
 #include <vector>
 #include "Doublelinkedlist.h"
+#include "Linkedlist.h"
+#include "SingleNode.h"
 
 void main() {
 	std::cout << "hello world" << std::endl;
 	std::cout << "hello ladaadad world" << std::endl;
-	Doublelinkedlist<std::string> stringlist;
+	/*Doublelinkedlist<std::string> stringlist;
 	//int * test = new int;
 	//*test = 2;
 	stringlist.InsertFirst("hej", 1);
 	//stringlist.InsertFirst(44, 2);
 
 	std::cout << stringlist.getHead() << " " << stringlist.getTail();
+
+	system("pause");*/
+
+	Linkedlist<int> intlist;
+	//int * test = new int;
+	//*test = 2;
+	intlist.InsertFirst(1, intlist.GetCount());
+	intlist.InsertFirst(2, intlist.GetCount());
+	//stringlist.InsertFirst(44, 2);
+
+	std::cout << intlist.getHead();
 
 	system("pause");
  
