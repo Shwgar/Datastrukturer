077d6172a71325ba3d674a25056f24885a4823e7
EmilJa <45233952+EmilJa@users.noreply.github.com>
2018-12-05 15:15:38 +0100

    Started working on Queue.

--- Datastrukturer/main.cpp
+++ Datastrukturer/main.cpp
@@ -1,7 +1,57 @@
 #include <iostream>
-#include <vector>
+#include "Queue.h"
 
-void main() {
-	std::cout << "hello world" << std::endl;
-	std::cout << "hello world" << std::endl;
+int main() {
+	Queue<int> queue(5);
+
+	queue.Enqueue(4);
+	queue.PrintFrontRear();
+
+	queue.Enqueue(43);
+	queue.PrintFrontRear();
+
+	queue.Enqueue(24);
+	queue.PrintFrontRear();
+
+	queue.Enqueue(87);
+	queue.PrintFrontRear();
+
+	queue.Enqueue(22);
+	queue.PrintFrontRear();
+
+	queue.Enqueue(11);
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+
+	int test = queue.Dequeue();
+	std::cout << "Dequeued " << queue.Dequeue() << std::endl;
+	queue.PrintFrontRear();
+	queue.PrintQueue();
+
+	return 0;
 }
