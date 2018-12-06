2e5714dc0fcb8c6b7f7359f7422de4c89b32e8e3
EmilJa <45233952+EmilJa@users.noreply.github.com>
2018-12-06 15:34:05 +0100

    Cleaned up the code.

--- Datastrukturer/main.cpp
+++ Datastrukturer/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-#include<string>
-#include <vector>
 #include "Doublelinkedlist.h"
 #include "Linkedlist.h"
 #include "SingleNode.h"
+#include "Queue.h"
 
-void main() {
-	std::cout << "hello world" << std::endl;
+int main() {
 
+
+	return 0;
 }
