1d5294e5fe52370632ccf8da93791030a394e58e
Martin <martin@swedishgamers.com>
2018-12-11 15:09:39 +0100

    Changed to support vectors, updated and added tests

--- Datastrukturer.test/LinkedList-Test.cpp
+++ Datastrukturer.test/LinkedList-Test.cpp
@@ -1,6 +1,8 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
 #include "gtest/gtest.h"
 #include "../Datastrukturer/Linkedlist.h"
 #include "../Datastrukturer/SingleNode.h"
+#include <vector>
 
 
 TEST(LinkedListTests, CheckThatAddedItemIsAtListHead) {
@@ -49,12 +51,38 @@ TEST(LinkedListTests, CheckIfSearchReturnsRightData) {
 	EXPECT_EQ(3, testReturn);
 }
 
-TEST(LinkedListTests, CheckIfSearchReturnsNoDataIfNotFound) {
+TEST(LinkedListTests, CheckIfSearchReturnsThrowIfNotFound) {
 	Linkedlist<int, int> list;
 	list.InsertFirst(9000, 1);
 	list.InsertFirst(2, 2);
 	list.InsertFirst(3, 3);
 	list.InsertFirst(4, 4);
-	int testReturn = list.Search(7);
-	EXPECT_EQ(NULL, testReturn);
+	EXPECT_THROW(int testReturn = list.Search(7), std::invalid_argument);
 }
+
+TEST(LinkedListTests, CheckIfSearchAfterDeleteReturnsNull) {
+	Linkedlist<int, int> list;
+	list.InsertFirst(9000, 1);
+	list.InsertFirst(2, 2);
+	list.InsertFirst(3, 3);
+	list.InsertFirst(4, 4);
+	list.DeleteKey(3);
+	EXPECT_THROW(int testReturn = list.Search(3), std::invalid_argument);
+}
+
+
+TEST(LinkedListTests, CheckIfSearchReturnsRightDataWithVectors) {
+	Linkedlist<std::vector<int>, int> list;
+	std::vector<int> vect1;
+	std::vector<int> vect2;
+	std::vector<int> vect3;
+	vect1.push_back(10);
+	vect2.push_back(20);
+	vect3.push_back(30);
+	list.InsertFirst(vect1, 1);
+	list.InsertFirst(vect2, 2);
+	list.InsertFirst(vect3, 3);
+	std::vector<int> testReturn = list.Search(3);
+	EXPECT_EQ(30, testReturn.at(0));
+}
+
